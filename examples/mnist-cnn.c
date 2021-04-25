fdd38f75b9438b5c95c50fcde46cb23991d0d958
Mrinal Ghosh <ghoshm@scc-201.scc.bu.edu>
2021-04-25 11:39:33 -0400

    Basic timing

--- examples/mnist-cnn.c
+++ examples/mnist-cnn.c
@@ -1,11 +1,35 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 #include "kann.h"
 #include "kann_extra/kann_data.h"
 
+/* -------------- Timing methods ------------- */
+double ts_ms(struct timespec ts) {
+    return ((((double)(ts.tv_sec)) * 1.0e9) + ((double)(ts.tv_nsec))) / (1.0e6);
+}
+
+struct timespec interval(struct timespec start, struct timespec stop) {
+    struct timespec temp;
+    if ((stop.tv_nsec - start.tv_nsec) < 0) {
+        temp.tv_sec = stop.tv_sec - start.tv_sec - 1;
+        temp.tv_nsec = 1000000000 + stop.tv_nsec - start.tv_nsec;
+    } else {
+        temp.tv_sec = stop.tv_sec - start.tv_sec;
+        temp.tv_nsec = stop.tv_nsec - start.tv_nsec;
+    }
+    return temp;
+}
+
+
 int main(int argc, char *argv[]) {
+    /* Timing */
+    struct timespec timer_start, timer_stop;
+    float runtime;
+    clock_gettime(CLOCK_REALTIME, &timer_start);
+
     kann_t *ann;
     kann_data_t *x, *y;
     char *fn_in = 0, *fn_out = 0;
@@ -32,6 +56,11 @@ int main(int argc, char *argv[]) {
         else if (c == 'v')
             frac_val = atof(optarg);
     }
+
+    printf("Built MNIST-CNN with training parameters:\n------------------------------------\n");
+    printf("Max epochs\t\t%d\n", max_epoch);
+    printf("Threads\t\t\t%d\n", n_threads);
+    printf("------------------------------------\n");
 
     if (argc - optind == 0 || (argc - optind == 1 && fn_in == 0)) {
         FILE *fp = stdout;
@@ -61,11 +90,13 @@ int main(int argc, char *argv[]) {
     y = argc - optind >= 2 ? kann_data_read(argv[optind + 1]) : 0;
 
     if (y) {  // training
+        printf("Starting training...\n\n");
         assert(y->n_col == 10);
         if (n_threads > 1) kann_mt(ann, n_threads, mini_size);
         kann_train_fnn1(ann, lr, mini_size, max_epoch, max_drop_streak, frac_val, x->n_row, x->x, y->x);
         if (fn_out) kann_save(fn_out, ann);
         kann_data_free(y);
+        printf("\n...training done\n");
     } else {  // applying
         int i, j, n_out;
         kann_switch(ann, 0);
@@ -85,5 +116,10 @@ int main(int argc, char *argv[]) {
 
     kann_data_free(x);
     kann_delete(ann);
+
+    clock_gettime(CLOCK_REALTIME, &timer_stop);
+    runtime = ts_ms(interval(timer_start, timer_stop));
+    printf("\nRuntime: %f (msec)\n", runtime);
+
     return 0;
 }
